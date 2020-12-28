d45b4293f5d74c294d42aa66906521979c0170aa
lihan <lihan@migu.cn>
2020-12-28 18:05:07 +0800

    .

---
+++ src/parser/mod.rs
@@ -0,0 +1,148 @@
+use crate::lexer::*;
+use crate::tree::*;
+use std::collections::HashMap;
+
+
+#[allow(dead_code)]
+pub enum SemanticsType {
+    Direct,    // 立即数
+    Calculate, // 计算
+    Assignment,
+    Compare,
+    MapSet,
+}
+
+#[allow(dead_code)]
+pub struct Parser <'a>  {
+    
+    /*
+    constructor(formula) {
+        this.lexer = new Lexer(formula)
+        this.const_map = {} // 变量值存储 ~ 都是全局的变量
+        this.func_map = {} // 函数表
+    }
+    */
+
+    pub lexer:  &'a mut Lexer,
+}
+
+impl <'a> Parser <'a> {
+    
+    /*
+     * term: factor { ("*" | "/") factor }
+     */
+
+    /*
+    term: factor { ("*" | "/") factor } 
+     | factor ">" factor
+     | factor ">=" factor
+     | factor "<" factor
+     | factor "<=" factor
+     | factor "==" factor
+    */
+
+    /*
+    parse_term() {
+        var left =  this.parse_factor()
+
+        var next = this.lexer.lookup()
+
+        if (/(>)|(>=)|(<)|(<=)|(==)|(!=)/.exec(next) != null) {
+            var token = this.lexer.pick()
+            var right = this.parse_factor()
+            var out = {
+                "oper": "cmp", // 比较
+                "token": token,
+                "left": left,
+                "right": right
+            }
+            return out
+        }
+
+        while ( this.lexer.lookup() == "*" ||  this.lexer.lookup() == "/") {
+            var oper =  this.lexer.pick()
+            var right = this.parse_factor()
+            var left = {
+                "left": left,
+                "oper": oper,
+                "right": right
+            }
+        }
+
+        return left
+    }
+    */
+
+    fn match_mul_div(&self, token_type: &TokenType) -> bool {
+        match token_type {
+            TokenType::MULTIP | TokenType::DIVIDER => {
+                return true;
+            },
+            _ => {return false;},
+        }
+    }
+
+    #[allow(dead_code)]
+    pub fn parse_term(&mut self) {
+        let left = self.parse_factor();
+        let next = self.lexer.lookup(0);
+        let token_type = &next.unwrap().token_type;
+
+        let literal = &next.unwrap().literal;
+
+        while self.match_mul_div(token_type) {
+            let token = self.lexer.pick();
+
+            match token {
+                None => {break;},
+                _ => {println!("##: {}", token.unwrap().literal)},
+            }
+
+            let right = self.parse_factor();
+
+            //left: Option<Box<Tree>>
+            let left_node = Some(Box::new(left.unwrap()));
+            let right_node = Some(Box::new(right.unwrap()));
+            let left = Tree {value: 0, token_type: TokenType::Number, semantics_type: SemanticsType::Calculate, left:left_node , right: right_node};
+            
+            let next = self.lexer.lookup(0);
+            let token_type = &next.unwrap().token_type;
+            let literal = &next.unwrap().literal;
+
+        }
+
+
+    }
+
+    /*
+    value: i32,
+    token_type: TokenType,
+    semantics_type: SemanticsType,
+    left: Option<Box<Tree>>,
+    right: Option<Box<Tree>>
+    */
+    #[allow(dead_code)]
+    pub fn parse_factor(&self) -> Option<Tree> {
+        let token = self.lexer.lookup(0);
+
+        let token_type = &token.unwrap().token_type;
+        
+        match token_type {
+            TokenType::Number => {
+                let int = token.unwrap().literal.parse::<i32>().unwrap();
+                println!("value = {}", int);
+                self.lexer.pick(); // 取数
+
+                let tree = Tree { value: 0, token_type: TokenType::Number, semantics_type: SemanticsType::Direct, left: None, right: None};
+                return Some(tree);
+            },
+            _ => {
+                return None;
+            },
+        }
+
+
+
+    }
+
+}
---
+++ src/tree/mod.rs
@@ -0,0 +1,112 @@
+use std::process;
+use std::borrow::Borrow;
+use crate::lexer::TokenType;
+use crate::parser::SemanticsType;
+
+// rust面向对象之继承: https://blog.csdn.net/u010766458/article/details/105403282/
+
+//#[derive(Debug, Default)]
+pub struct Tree {
+    pub value: i32,
+    pub token_type: TokenType,
+    pub semantics_type: SemanticsType,
+    pub left: Option<Box<Tree>>,
+    pub right: Option<Box<Tree>>
+}
+
+pub struct CompareNode  {
+    tree: Tree,
+    name: String,
+    comparator: String,
+}
+
+impl Tree {
+    /*
+    pub fn get_val(&self) -> i32 {
+        return self.value;
+    }
+
+    pub fn set_val(&mut self, val: i32) -> i32 {
+        self.value = val;
+        return self.value;
+    }
+
+    pub fn insert(&mut self, dir: &String, val: Tree) {
+        assert!(dir == "left" || dir == "right");
+        match dir.as_ref() {
+            "left" => self.left = Some(Box::new(val)),
+            "right" => self.right = Some(Box::new(val)),
+            _ => { 
+                println!("Insert Error: only left and right supported");
+                process::exit(1);
+            }
+        }
+    }
+
+    pub fn delete(&mut self, dir: &String) {
+        assert!(dir == "left" || dir == "right");
+        match dir.as_ref() {
+                "left" => self.left = None,
+                "right" => self.right = None,
+                 _ => { 
+                    println!("Insert Error: only left and right supported");
+                    process::exit(1);
+                }
+        }
+    }
+    */
+}
+
+// 原始的非消耗性遍历:
+// fn traverse(tree: &Tree) {
+//     println!("Node Value: {:?}", tree.value);
+//     if tree.left.is_some() {
+//         // cannot move out of borrowed content
+//         // 首先 unwrap 是一个消耗性操作
+//         // 这是由于 unwrap 函数造成?  as_ref 也不行
+//         traverse((tree.left.as_ref().map(|x| **x).unwrap()).borrow());
+//     }
+//     // if tree.right.is_some() {
+//     //     // cannot move out of borrowed content
+//     //     traverse(tree.right.unwrap().borrow());
+//     // }
+// }
+
+// 非消耗性遍历
+fn traverse(tree: &Tree) {
+    println!("Node Value: {:?}", tree.value);
+    match tree.left {
+        Some(ref x) => traverse(x),
+        _ => {}
+    }
+    match tree.right {
+        Some(ref x) => traverse(x),
+        _ => {}
+    }
+}
+
+// 消耗性遍历：
+// fn traverse(tree: Tree) {
+//     println!("Node Value: {:?}", tree.value);
+//     if tree.left.is_some() {
+//         traverse(*tree.left.unwrap()); // 手动解引用
+//     }
+//     if tree.right.is_some() {
+//         traverse(*tree.right.unwrap()); // 手动解引用
+//     }
+// }
+
+pub fn tree_main() {
+    /*
+    println!("begin rust tree test:");
+    let mut tree = Tree { value : 12, ..Default::default() };
+    let mut left = Tree { value : 121, ..Default::default() };
+    tree.insert(&String::from("left"), left);
+    let mut right = Tree { value : 122, ..Default::default() };
+    tree.insert(&String::from("right"), right);
+    // tree.delete(&String::from("right"));
+    // println!("Tree val: {:?}", left.get_val()); 不能这样写，所有权已经被移动
+    traverse(&tree);
+    // traverse(tree);
+    */
+}
